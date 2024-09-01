a0948c29f14880da8e0c288246ee937c6a93d46c
Leaves333 <canchenli@gmail.com>
2024-09-01 13:27:14 -0500

    second commit?

--- abc368d.cpp
+++ abc368d.cpp
@@ -6,12 +6,12 @@ int main() {
 	int n, k;
 	cin >> n >> k;
 
-	set<int> edges[n];
+	unordered_set<int> edges[n];
 	for (int i = 0; i < n - 1; i++) {
 		int a, b;
 		cin >> a >> b;
-		edges[a].insert(b);
-		edges[b].insert(a);
+		edges[a - 1].insert(b - 1);
+		edges[b - 1].insert(a - 1);
 	}
 
 	set<int> required;
@@ -21,21 +21,16 @@ int main() {
 		required.insert(x - 1);
 	}
 	
-	vector<int> degree;
-	for (auto v : edges) {
-		degree.push_back(v.size());
-	}
-
 	vector<int> queue;
-	for (int i = 0; i < degree.size(); i++) {
-		if (degree[i] == 1) {
+	for (int i = 0; i < n; i++) {
+		if (edges[i].size() == 1) {
 			queue.push_back(i);
 		}
 	}
 
 	int ans = n;
 	for (int i = 0; i < queue.size(); i++) {
-
+		
 		int v = queue[i];
 		if (required.count(v) > 0) {
 			continue;
@@ -44,10 +39,10 @@ int main() {
 		int adj = *edges[v].begin();
 		edges[adj].erase(v);
 		edges[v].clear();
+		ans--;
 
 		if (edges[adj].size() == 1) {
 			queue.push_back(adj);
-			ans--;
 		}
 
 	}

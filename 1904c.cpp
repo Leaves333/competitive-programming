f62ba494d2dcf08e9041f38b7464bfb6594a9c21
Leaves333 <canchenli@gmail.com>
2025-04-24 11:59:52 -0500

    solve 1400 problem

--- 1904c.cpp
+++ 1904c.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #pragma optimize("O3")
 
 #include <bits/stdc++.h>
@@ -26,22 +27,36 @@ int main() {
             cin >> nums[i];
         }
 
+        sort(nums.begin(), nums.end());
         if (k >= 3) {
             cout << 0 << endl;
         } else if (k == 0) {
-            ll ans = LLONG_MAX;
-            for (int i = 0; i < n; i++) {
-                ans = min(ans, nums[i]);
-            }
-            cout << ans << endl;
-        } else {
-            sort(nums.begin(), nums.end());
+            cout << nums[0] << endl;
+        } else if (k == 1) {
             ll ans = nums[0];
             for (int i = 1; i < n; i++) {
                 ans = min(ans, nums[i] - nums[i-1]);
-                ans = min(ans, nums[i]);
             }
             cout << ans << endl;
+        } else if (k == 2) {
+
+            ll ans = nums[0];
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    ll val = nums[j] - nums[i];
+                    auto it = lower_bound(nums.begin(), nums.end(), val);
+                    if (it != nums.end())
+                        ans = min(ans, *it - val);
+                    if (it != nums.begin())
+                        ans = min(ans, val - *(it - 1));
+                }
+            }
+
+            for (int i = 1; i < n; i++) {
+                ans = min(ans, nums[i] - nums[i-1]);
+            }
+            cout << ans << endl;
+
         }
     }
 }

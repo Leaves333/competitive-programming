6d99927066ac248066960a36e15d70ab397c0b8f
Leaves333 <canchenli@gmail.com>
2025-05-19 22:25:39 -0500

    upsolve median splits

--- 2103c.cpp
+++ 2103c.cpp
@@ -32,31 +32,13 @@ int main() {
             psum[i + 1] = psum[i] + nums[i];
         }
         
-        vi max_dp(n + 1), min_dp(n + 1);
+        vi max_dp(n), min_dp(n);
         max_dp[n-1] = psum[n-1];
         min_dp[n-1] = psum[n-1];
-        for (int i = n-2; i >= 0; i--) {
+        for (int i = n-2; i > 0; i--) {
             max_dp[i] = max(max_dp[i+1], psum[i]);
             min_dp[i] = min(min_dp[i+1], psum[i]);
         }
-
-        /*cout << "here's psum: ";*/
-        /*for (auto x : psum) {*/
-        /*    cout << x << " ";*/
-        /*}*/
-        /*cout << endl;*/
-
-        /*cout << "here's max_dp: ";*/
-        /*for (auto x : max_dp) {*/
-        /*    cout << x << " ";*/
-        /*}*/
-        /*cout << endl;*/
-        
-        /*cout << "here's min_dp: ";*/
-        /*for (auto x : min_dp) {*/
-        /*    cout << x << " ";*/
-        /*}*/
-        /*cout << endl;*/
 
         bool good = false;
         for (int i = 1; i < n - 1; i++) {
@@ -64,8 +46,6 @@ int main() {
             int first = psum[i];
             int second = max_dp[i+1] - psum[i];
             int third = psum[n] - max_dp[i+1];
-
-            /*cout << "attempted: " << first << " " << second << " " << third << endl;*/
 
             int count = (first >= 0) + (second >= 0) + (third >= 0);
             if (count >= 2) {
@@ -76,7 +56,43 @@ int main() {
             second = min_dp[i+1] - psum[i];
             third = psum[n] - min_dp[i+1];
 
-            /*cout << "attempted: " << first << " " << second << " " << third << endl;*/
+            count = (first >= 0) + (second >= 0) + (third >= 0);
+            if (count >= 2) {
+                good = true;
+                break;
+            }
+        }
+
+        // do the problem again but reverse the array
+        reverse(nums.begin(), nums.end());
+        psum.clear();
+        for (int i = 0; i < n; i++) {
+            psum[i + 1] = psum[i] + nums[i];
+        }
+        
+        max_dp.clear();
+        min_dp.clear();
+        max_dp[n-1] = psum[n-1];
+        min_dp[n-1] = psum[n-1];
+        for (int i = n-2; i > 0; i--) {
+            max_dp[i] = max(max_dp[i+1], psum[i]);
+            min_dp[i] = min(min_dp[i+1], psum[i]);
+        }
+
+        for (int i = 1; i < n - 1; i++) {
+
+            int first = psum[i];
+            int second = max_dp[i+1] - psum[i];
+            int third = psum[n] - max_dp[i+1];
+
+            int count = (first >= 0) + (second >= 0) + (third >= 0);
+            if (count >= 2) {
+                good = true;
+                break;
+            }
+
+            second = min_dp[i+1] - psum[i];
+            third = psum[n] - min_dp[i+1];
 
             count = (first >= 0) + (second >= 0) + (third >= 0);
             if (count >= 2) {

033462343b215f2fb0b0f2fc21ffcb9d9e5c17a6
Leaves333 <canchenli@gmail.com>
2025-05-21 10:16:01 -0500

    struggle with problem d

--- c.cpp
+++ c.cpp
@@ -14,11 +14,74 @@ typedef vector<vll> vvll;
 typedef vector<bool> vb;
 typedef vector<vb> vvb;
 
+struct DSU {
+
+    vi parent, size;
+
+    void init_dsu(int n) {
+        parent = vi(n);
+        size = vi(n, 1);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find_set(int v) {
+        if (v == parent[v])
+            return v;
+        return parent[v] = find_set(parent[v]);
+    }
+
+    void union_sets(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a == b)
+            return;
+        if (size[a] < size[b])
+            swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+    }
+};
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    int t; cin >> t;
-    while (t--) {
-        
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    DSU first, second;
+    first.init_dsu(n + 1);
+    second.init_dsu(n + 1);
+
+    for (int i = 0; i < a; i++) {
+        int x, y;
+        cin >> x >> y;
+        first.union_sets(x, y);
     }
+
+    for (int i = 0; i < b; i++) {
+        int x, y;
+        cin >> x >> y;
+        second.union_sets(x, y);
+    }
+
+    vector<pii> ans;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            if (first.find_set(i) == first.find_set(j))
+                continue;
+            if (second.find_set(i) == second.find_set(j))
+                continue;
+            ans.push_back({i, j});
+            first.union_sets(i, j);
+            second.union_sets(i, j);
+        }
+    }
+
+    cout << ans.size() << endl;
+    for (auto x : ans) {
+        cout << x.first << " " << x.second << endl;
+    }
+
 }
--- d.cpp
+++ d.cpp
@@ -14,11 +14,35 @@ typedef vector<vll> vvll;
 typedef vector<bool> vb;
 typedef vector<vb> vvb;
 
+// make a tree:
+// preorder traversal is the given order
+// children of each node must be labeled 1, 2, 3 ...
+
+void build_graph(int x, const vi &nums, vvi &edges, vi &color) {
+
+    // find all possible locations to put the new node
+    // if there are no good candidates, unwind the stack
+
+    for (int i = 0; i < x; i++) {
+        
+    }
+
+}
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
     int t; cin >> t;
     while (t--) {
-        
+
+        int n; cin >> n;
+        vi nums(n + 1);
+        for (int i = 1; i <= n; i++) {
+            cin >> nums[i];
+        }
+
+        vvi edges(n);
+        vi color(n);
+
     }
 }
